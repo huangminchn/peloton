18837845f454702da0d04957d366ece02054c723
Min Huang <huangminchn@yahoo.com>
2017-11-16 03:20:08 -0500

    more log

--- src/codegen/compilation_context.cpp
+++ src/codegen/compilation_context.cpp
@@ -73,8 +73,9 @@ void CompilationContext::GeneratePlan(QueryCompiler::CompileStats *stats) {
   timer.Start();
 
   // First we prepare the translators for all the operators in the tree
+  printf("before prepare\n");
   Prepare(query_.GetPlan(), main_pipeline_);
-
+  printf("after prepare\n");
   if (stats != nullptr) {
     timer.Stop();
     stats->setup_ms = timer.GetDuration();
@@ -82,7 +83,7 @@ void CompilationContext::GeneratePlan(QueryCompiler::CompileStats *stats) {
     timer.Start();
   }
 
-  LOG_TRACE("Main pipeline: %s", main_pipeline_.GetInfo().c_str());
+  LOG_INFO("Main pipeline: %s", main_pipeline_.GetInfo().c_str());
 
   // Generate the helper functions the query needs
   GenerateHelperFunctions();
@@ -91,6 +92,7 @@ void CompilationContext::GeneratePlan(QueryCompiler::CompileStats *stats) {
   llvm::Function *init = GenerateInitFunction();
 
   // Generate the plan() function
+  printf("before generating plan in query_\n");
   llvm::Function *plan = GeneratePlanFunction(query_.GetPlan());
 
   // Generate the  tearDown() function
