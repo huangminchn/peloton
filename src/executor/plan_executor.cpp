35d4fb0aa059100d0f962dcfe1f70a2edb99f265
Min Huang <huangminchn@yahoo.com>
2017-11-17 23:15:57 -0500

    support point query in code gen

--- src/executor/plan_executor.cpp
+++ src/executor/plan_executor.cpp
@@ -101,16 +101,21 @@ void PlanExecutor::ExecutePlan(
   // Perform binding
   planner::BindingContext context;
   plan->PerformBinding(context);
-  printf("after binding\n");
   // Prepare output buffer
   std::vector<oid_t> columns;
-  plan->GetOutputColumns(columns);
+  if (plan->GetPlanNodeType() == PlanNodeType::INDEXSCAN) {
+    // must use GetColumnIds() function in index scan plan node
+    // to get output columns, because the column_ids_ variable
+    // in IndexScanPlan class is different from the column_ids_
+    // variable in its parent class AbstractScan!
+    columns = (dynamic_cast<planner::IndexScanPlan &>(*plan)).GetColumnIds();
+  } else {
+    plan->GetOutputColumns(columns);
+  }
   codegen::BufferingConsumer consumer{columns, context};
 
   // Compile & execute the query
   codegen::QueryCompiler compiler;
-  printf("before compiling\n");
-  printf("plan info = %s\n", plan->GetInfo().c_str());
   auto query = compiler.Compile(*plan, consumer);
   query->Execute(*txn, executor_context.get(),
                  reinterpret_cast<char *>(consumer.GetState()));
