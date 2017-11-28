68fb008dc7306497c97203914752d64c66e500b6
Min Huang <huangminchn@yahoo.com>
2017-11-27 19:10:21 -0500

    remove timer

--- src/executor/plan_executor.cpp
+++ src/executor/plan_executor.cpp
@@ -23,7 +23,6 @@
 #include "executor/executors.h"
 #include "storage/tuple_iterator.h"
 #include "settings/settings_manager.h"
-#include "common/timer.h"
 
 namespace peloton {
 namespace executor {
@@ -56,8 +55,6 @@ void PlanExecutor::ExecutePlan(
 
   if (!settings::SettingsManager::GetBool(settings::SettingId::codegen)
       || !codegen::QueryCompiler::IsSupported(*plan)) {
-    Timer<> timer;
-    timer.Start();
     bool status;
     std::unique_ptr<executor::AbstractExecutor> executor_tree(
         BuildExecutorTree(nullptr, plan.get(), executor_context.get()));
@@ -95,16 +92,10 @@ void PlanExecutor::ExecutePlan(
     p_status.m_result = ResultType::SUCCESS;
     p_status.m_result_slots = nullptr;
     CleanExecutorTree(executor_tree.get());
-    timer.Stop();
-    LOG_INFO("[INTERPRETER] execution takes %.8lfs", timer.GetDuration());
-    // clear the output for index scan to skip the network
-    if (plan->GetPlanNodeType() == PlanNodeType::INDEXSCAN) {
-      result.clear();
-    }
     return;
   }
 
-  LOG_INFO("Compiling and executing query ...");
+  LOG_TRACE("Compiling and executing query ...");
   // Perform binding
   planner::BindingContext context;
   plan->PerformBinding(context);
