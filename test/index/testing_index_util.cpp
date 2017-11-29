50029a469da01d519bacfc5317abf963009ae68e
Min Huang <huangminchn@yahoo.com>
2017-11-28 19:39:45 -0500

    print memory usage

--- test/index/testing_index_util.cpp
+++ test/index/testing_index_util.cpp
@@ -27,6 +27,13 @@
 #include "index/index.h"
 #include "executor/testing_executor_util.h"
 
+/*
+
+#include "sys/types.h"
+#include "sys/sysinfo.h"
+
+ */
+
 namespace peloton {
 namespace test {
 
@@ -223,11 +230,54 @@ void TestingIndexUtil::MultiThreadedInsertTest(const IndexType index_type) {
       TestingIndexUtil::BuildIndex(index_type, false), DestroyIndex);
   // const catalog::Schema *key_schema = index->GetKeySchema();
 
+  /*
+  struct sysinfo memInfo;
+
+  sysinfo (&memInfo);
+  long long totalVirtualMem = memInfo.totalram;
+//Add other values in next statement to avoid int overflow on right hand side...
+  totalVirtualMem += memInfo.totalswap;
+  totalVirtualMem *= memInfo.mem_unit;
+  long long virtualMemUsed = memInfo.totalram - memInfo.freeram;
+//Add other values in next statement to avoid int overflow on right hand side...
+  virtualMemUsed += memInfo.totalswap - memInfo.freeswap;
+  virtualMemUsed *= memInfo.mem_unit;
+
+  long long totalPhysMem = memInfo.totalram;
+//Multiply in next statement to avoid int overflow on right hand side...
+  totalPhysMem *= memInfo.mem_unit;
+  long long physMemUsed = memInfo.totalram - memInfo.freeram;
+//Multiply in next statement to avoid int overflow on right hand side...
+  physMemUsed *= memInfo.mem_unit;
+  printf("before populating map:%lld %lld %lld %lld\n", totalVirtualMem, virtualMemUsed, totalPhysMem, physMemUsed);
+   */
+
   index::Index &index_r = *index.get();
   if (!map_populated) {
     PopulateMap(index_r);
     map_populated = true;
   }
+
+
+  /*
+  sysinfo (&memInfo);
+  totalVirtualMem = memInfo.totalram;
+//Add other values in next statement to avoid int overflow on right hand side...
+  totalVirtualMem += memInfo.totalswap;
+  totalVirtualMem *= memInfo.mem_unit;
+  virtualMemUsed = memInfo.totalram - memInfo.freeram;
+//Add other values in next statement to avoid int overflow on right hand side...
+  virtualMemUsed += memInfo.totalswap - memInfo.freeswap;
+  virtualMemUsed *= memInfo.mem_unit;
+
+  totalPhysMem = memInfo.totalram;
+//Multiply in next statement to avoid int overflow on right hand side...
+  totalPhysMem *= memInfo.mem_unit;
+  physMemUsed = memInfo.totalram - memInfo.freeram;
+//Multiply in next statement to avoid int overflow on right hand side...
+  physMemUsed *= memInfo.mem_unit;
+  printf("after populating map: %lld %lld %lld %lld\n", totalVirtualMem, virtualMemUsed, totalPhysMem, physMemUsed);
+  */
 
   // Parallel Test
   size_t num_threads = 20;
@@ -241,6 +291,27 @@ void TestingIndexUtil::MultiThreadedInsertTest(const IndexType index_type) {
                      pool, scale_factor, num_rows);
   timer.Stop();
   printf("%lu tuples elapsed time = %.5lf\n", num_threads*scale_factor*num_rows, timer.GetDuration());
+
+
+  /*
+  sysinfo (&memInfo);
+  totalVirtualMem = memInfo.totalram;
+//Add other values in next statement to avoid int overflow on right hand side...
+  totalVirtualMem += memInfo.totalswap;
+  totalVirtualMem *= memInfo.mem_unit;
+  virtualMemUsed = memInfo.totalram - memInfo.freeram;
+//Add other values in next statement to avoid int overflow on right hand side...
+  virtualMemUsed += memInfo.totalswap - memInfo.freeswap;
+  virtualMemUsed *= memInfo.mem_unit;
+
+  totalPhysMem = memInfo.totalram;
+//Multiply in next statement to avoid int overflow on right hand side...
+  totalPhysMem *= memInfo.mem_unit;
+  physMemUsed = memInfo.totalram - memInfo.freeram;
+//Multiply in next statement to avoid int overflow on right hand side...
+  physMemUsed *= memInfo.mem_unit;
+  printf("after inserting tuples: %lld %lld %lld %lld\n", totalVirtualMem, virtualMemUsed, totalPhysMem, physMemUsed);
+*/
 
   index->ScanAllKeys(location_ptrs);
   printf("tuple size = %lu\n", location_ptrs.size());
